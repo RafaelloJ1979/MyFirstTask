4a763beb7bb14a31fe0a00969d400ece55214395
RafaelloJ1979 <rafal1979jaskowski@gmail.com>
2021-02-25 18:13:49 +0100

    dodane tab1234

---
+++ scrypt.js
@@ -0,0 +1,30 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Rafał Jaśkowski";
+    document.getElementById("demo").style.color = 'rgba(1, 128, 187, 0.685)';
+    document.getElementById("demo").style.fontSize = '60px';
+}
+function getColor() {
+    const getDemo = document.getElementById("demo")
+    document.getElementById("h1").innerHTML = "Rafał Jaśkowski";
+    document.getElementById("h1").style.fontSize = '60px';
+    document.getElementById("getcolor").style.color = 'rgba(421, 363, 17, 1.678)';
+    document.getElementById("test").innerHTML = "tak dla sprawdzenia";
+}
+
+function openCity1() {
+    document.getElementById("tab1").innerHTML = "zmieniam na tekst ;)";
+}
+function openCity2() {
+    document.getElementById("tab2").innerHTML = "zmiana koloru " ;
+}
+
+function openCity3() {
+    document.getElementById("tab3").innerHTML = "tworzymy inputa";
+    document.createTextNode('pole '+i+': ');
+}
+
+function openCity4() {
+    document.getElementById("tab4").innerHTML = "czwarty jak coś wpiszesz s inpucie i wycisniesz ten 4ty to żeby odmieniło jego tekst na ten z tego co jest w inpucie";
+}
+
+
